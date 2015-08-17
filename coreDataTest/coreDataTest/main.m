d3cc51dcc62c69795d2004c91e57d731e392f49c
John Bogil <johncbogil@gmail.com>
2015-08-17 12:59:00 -0400

    Initial commit

---
+++ coreDataTest/coreDataTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coreDataTest
+//
+//  Created by John Bogil on 8/17/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
